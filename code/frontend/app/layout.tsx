ae8b68a38b760d24c0fc9587c9c72eb905a277fe
nazwasyakuru <nazwa_syakuru@teknokrat.ac.id>
2025-11-22 16:31:09 +0700

    nazwa: menghapus file trackingstepper.tsx dan navbar.tsx di folder components, mengubah layout.tsx, mengubah page.tsx, mengubah package.json

--- code/frontend/app/layout.tsx
+++ code/frontend/app/layout.tsx
@@ -1,32 +1,14 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
+export const metadata = {
   title: "Clean Route Laundry",
-  description: "Aplikasi Laundry dengan Tracking",
+  description: "Aplikasi Laundry dengan fitur Tracking",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="id">
+      <body className="bg-gray-100 min-h-screen">
         {children}
       </body>
     </html>
