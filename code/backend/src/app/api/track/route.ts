1d0a307128ad1310312c5b0c427aa6bd7d27df8d
ferdianalfarizi6 <ferdian_alfarizi@teknokrat.ac.id>
2025-12-01 10:54:49 +0700

    ferdianalfarizi6: menambahkan metode post

--- code/backend/src/app/api/track/route.ts
+++ code/backend/src/app/api/track/route.ts
@@ -4,7 +4,7 @@ import jwt from "jsonwebtoken";
 
 const prisma = new PrismaClient();
 
-// ===== VERIFY TOKEN =====
+// verify JWT token
 function verifyToken(req: Request) {
     const auth = req.headers.get("authorization");
     if (!auth) return null;
@@ -15,4 +15,31 @@ function verifyToken(req: Request) {
         return null;
     }
 }
+
+// tambah tracking admin
+export async function POST(req: Request) {
+    try {
+        const user = verifyToken(req); 
+        if (!user) {
+            return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+        }
+        const data = (await req.json()) as {
+            orderId: number;
+            status: string;
+        };
+        const { orderId, status } = data;
+        if (!orderId || !status) {
+            return NextResponse.json(
+                { message: "status track wajib di isi" },
+                { status: 400 }
+            );
+        }
+        
+    }
+    catch (err) {
+        console.log(err);
+        return NextResponse.json({ error: "Internal server error" }, { status: 500 });
+    }
+}
+
 
