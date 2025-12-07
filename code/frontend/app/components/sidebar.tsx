52070bc58894c72d14df4b07ce79e5c40466f457
nazwasyakuru <nazwa_syakuru@teknokrat.ac.id>
2025-12-07 20:26:05 +0700

    nazwa: Tambah komponen sidebar dengan header menu

---
+++ code/frontend/app/components/sidebar.tsx
@@ -0,0 +1,6 @@
+"use client";
+import Link from "next/link";
+export default function Sidebar() {
+  return (
+    <aside className="w-60 bg-white rounded p-4 shadow">
+      <div className="mb-4 font-semibold">Menu</div>
