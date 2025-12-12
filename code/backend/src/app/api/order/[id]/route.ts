2c8880b4963ead459264711f5ef9639efdda6ed1
ferdianalfarizi6 <ferdian_alfarizi@teknokrat.ac.id>
2025-12-13 00:09:12 +0700

    ferdianalfarizi6: order/[id] add get all order derail

--- code/backend/src/app/api/order/[id]/route.ts
+++ code/backend/src/app/api/order/[id]/route.ts
@@ -23,3 +23,43 @@ function verifyToken(req: Request): TokenPayload | null {
     return null;
   }
 }
+
+// ===== GET ORDER DETAIL =====
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const id = Number(params.id);
+
+    const order = await prisma.order.findUnique({
+      where: { id },
+    });
+
+    if (!order) {
+      return NextResponse.json(
+        { message: "Order tidak ditemukan" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json({ order }, { status: 200 });
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json(
+      { error: "Terjadi kesalahan" },
+      { status: 500 }
+    );
+  }
+}
+
+
+type OrderUpdatePayload = {
+  name?: string;
+  phone?: string;
+  address?: string;
+  serviceType?: string;
+  weight?: number;
+  totalPrice?: number;
+};
+
