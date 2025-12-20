4b83e45cf8fcb2cde2c372476bc3b3e5d4dd456b
nazwasyakuru <nazwa_syakuru@teknokrat.ac.id>
2025-12-20 13:25:18 +0700

    nazwa: mengubah isi file page.tsx di folder order

--- code/frontend/app/orders/page.tsx
+++ code/frontend/app/orders/page.tsx
@@ -1,22 +1,20 @@
-import Navbar from "../components/navbar";
 import Link from "next/link";
 
 export default function OrdersPage() {
   return (
-    <>
-      <Navbar />
-      <div className="p-6">
-        <h2 className="text-xl font-semibold mb-3">Daftar Order</h2>
+    <div>
+      <h2 className="text-xl font-semibold mb-3 text-teal-600">
+        Daftar Order
+      </h2>
 
-        <div className="bg-white shadow p-4 rounded">
-          <Link
-            href="/orders/1"
-            className="block p-3 border-b hover:bg-gray-100"
-          >
-            Order #1 — Status: Penjemputan
-          </Link>
-        </div>
+      <div className="bg-white shadow p-4 rounded">
+        <Link
+          href="/orders/1"
+          className="block p-3 border-b hover:bg-gray-100"
+        >
+          Order #1 — Status: Penjemputan
+        </Link>
       </div>
-    </>
+    </div>
   );
 }
