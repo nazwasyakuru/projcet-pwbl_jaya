c097317c05d41dde7dca0f3bb516c840648ec62c
nazwasyakuru <nazwa_syakuru@teknokrat.ac.id>
2025-12-20 16:24:11 +0700

    nazwa: menambahkan isi file layout untuk page admin

---
+++ code/frontend/app/(dashboard)/layout.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import Sidebar from "../components/sidebar";
+import Navbar from "../components/navbar";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex min-h-screen bg-gray-100">
+      <Sidebar />
+      <div className="flex-1 flex flex-col">
+        <Navbar />
+        <main className="flex-1 p-6">{children}</main>
+      </div>
+    </div>
+  );
+}
