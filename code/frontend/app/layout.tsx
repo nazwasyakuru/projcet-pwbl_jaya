e1f0cc5e70499a00cf4de0b69154f9563b8764c5
nazwasyakuru <nazwa_syakuru@teknokrat.ac.id>
2025-12-20 16:27:36 +0700

    nazwa: memperbaiki file layout.tsx

--- code/frontend/app/layout.tsx
+++ code/frontend/app/layout.tsx
@@ -1,8 +1,4 @@
-"use client";
-
 import "./globals.css";
-import Sidebar from "./components/sidebar";
-import Navbar from "./components/navbar";
 
 export default function RootLayout({
   children,
@@ -11,18 +7,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="id">
-      <body>
-        <div className="flex min-h-screen bg-gray-100">
-          {/* Sidebar */}
-          <Sidebar />
-
-          {/* Konten kanan */}
-          <div className="flex-1 flex flex-col">
-            <Navbar />
-            <main className="flex-1 p-6">{children}</main>
-          </div>
-        </div>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
